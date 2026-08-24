11cfe55ef2eb0276bb012bfe716f9783f9973d34
sabareeshraam17 <sabareeshraam17@gmail.com>
2026-08-24 13:18:23 +0530

    Update admin-orders.js

--- api/admin-orders.js
+++ api/admin-orders.js
@@ -1,4 +1,4 @@
-const { ensureSchema } = require("../lib/db");
+const { ensureSchema } = require("./_lib/db");
 const { isAuthed } = require("../lib/auth");
 
 module.exports = async (req, res) => {
